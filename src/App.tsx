ca2496033fade15935f3e5c1bdbb5402608bfd1f
Gyanendra Kumar <gyanendrak874@gmail.com>
2025-04-29 16:47:05 +0530

    Added dark mode support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/provider/theme-provider"
+import { Button } from "./components/ui/button"
 
 function App() {
-
   return (
-    // <div className="flex flex-col items-center justify-center min-h-svh">
-    <Button variant="outline">Outline</Button>
-    // </div>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div>
+        <Button>Button</Button>
+      </div>
+    </ThemeProvider>
   )
 }
 
