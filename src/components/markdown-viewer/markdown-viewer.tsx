d5c967e8473cb88eabc9eec26dd26cad90c763c0
Gyanendra Kumar <gyanendrak874@gmail.com>
2025-04-30 11:04:06 +0530

    Added accordian

--- src/components/markdown-viewer/markdown-viewer.tsx
+++ src/components/markdown-viewer/markdown-viewer.tsx
@@ -3,6 +3,10 @@ import { useTheme } from '../provider/theme-provider';
 import ReactMarkdown from 'react-markdown';
 import DOMPurify from 'dompurify';
 import rehypeRaw from 'rehype-raw';
+import { Badge } from '../ui/badge';
+import { Icon } from '../ui/icon';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../ui/accordion';
+import { hintContent, topics } from '@/lib/constant';
 
 interface MarkdownViewerProps {
   content: string;
@@ -11,33 +15,64 @@ interface MarkdownViewerProps {
 const MarkdownViewer = ({ content }: MarkdownViewerProps) => {
 
   const [markdownContent, setMarkdownContent] = useState(content);
-  
+
   // Configure DOMPurify to allow HTML tags
   const sanitizedMarkdown = DOMPurify.sanitize(markdownContent, {
     USE_PROFILES: { html: true }
   });
-  
+
   console.log(sanitizedMarkdown);
-  
+
   useEffect(() => {
     setMarkdownContent(content);
   }, [content]);
 
   // Determine text color based on theme
   const isDarkTheme = useTheme().theme === 'dark'
-  
+
   return (
-    <div 
+    <div
       className="h-[calc(100%-1.7rem)] overflow-auto p-4 pb-10"
       style={{
         color: isDarkTheme ? '#e0e0e0' : '#333333',
         backgroundColor: isDarkTheme ? '#1e1e1e' : '#ffffff'
       }}
     >
+      <div className='flex items-center justify-start mb-4 gap-2'>
+        <Badge variant={'outline'} className='rounded-full px-3 py-1 text-easy bg-muted'>Easy</Badge>
+        <Badge variant={'outline'} className='rounded-full px-3 py-1'><Icon name='TagIcon' /> Topics</Badge>
+        <Badge variant={'outline'} className='rounded-full px-3 py-1'><Icon name='LightbulbIcon' /> Hint</Badge>
+      </div>
       <div className="markdown-content">
         <ReactMarkdown rehypePlugins={[rehypeRaw]} >
           {sanitizedMarkdown}
         </ReactMarkdown>
+      </div>
+      {/* Topic Section */}
+      <div className='border-b last:border-b-0'>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value={`item-1`} className='border-b'>
+            <AccordionTrigger className='hover:no-underline cursor-pointer'>
+            <div className='flex gap-5 items-center '><Icon name='TagIcon' className='h-4 w-4' />{"Topics"}</div>
+            </AccordionTrigger>
+            <AccordionContent className='ml-10 gap-2 flex'>{topics.map(item => <Badge className='rounded-full px-3 py-1' variant={'secondary'} key={item.id}>{item.name}</Badge>)}</AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+      {/* Hint Section */}
+      <div>
+        <Accordion type="single" collapsible className="w-full">
+          {
+            hintContent.map((item) => {
+              return (
+                <AccordionItem key={item.id} value={`item-${item.id}`} className='border-b'>
+                  <AccordionTrigger className='hover:no-underline cursor-pointer'><div className='flex gap-5 items-center'><Icon name='LightbulbIcon' className='h-4 w-4' />{item.title}</div></AccordionTrigger>
+                  <AccordionContent className='ml-10'>{item.content}</AccordionContent>
+                </AccordionItem>
+              )
+            })
+          }
+        </Accordion>
       </div>
     </div>
   );
--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -67,3 +67,13 @@ export const DEFAULT_CODE_BY_LANGUAGE: Record<Language['id'], string> = {
     cpp: '// Write your C++ code here\n\n#include <iostream>\n\nint main() {\n    std::cout << "Hello World!" << std::endl;\n    return 0;\n}\n',
     csharp: '// Write your C# code here\n\npublic class Solution {\n    public static void Main(string[] args) {\n        System.Console.WriteLine("Hello World!");\n    }\n}\n',
 };
+
+export const hintContent = [
+    { id: 1, type: "hint", title: "Hint 1", content: "A really brute force way would be to search for all possible pairs of numbers but that would be too slow. Again, it's best to try out brute force solutions for just for completeness. It is from these brute force solutions that you can come up with optimizations." },
+    { id: 2, type: "hint", title: "Hint 2", content: "So, if we fix one of the numbers, say x, we have to scan the entire array to find the next number y which is value - x where value is the input parameter. Can we change our array somehow so that this search becomes faster?" },
+    { id: 3, type: "hint", title: "Hint 3", content: "The second train of thought is, without changing the array, can we use additional space somehow? Like maybe a hash map to speed up the search?" }
+]
+export const topics = [
+    {id: 1, name: "Array"},
+    {id: 2, name: "Hash Table"},
+]
