8a4ebaf0203ec8cdb4f50e40a26684a9d7ab99d3
Gyanendra Kumar <gyanendrak874@gmail.com>
2025-05-01 14:14:16 +0530

    Added dynamic tags and hints

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,46 +1,5 @@
 import { Language, Theme } from "@/components/code-editor/types";
 
-export const sampleMarkdown = `
-### Problem Statement
-
-Given an array of integers \`nums\` and an integer \`target\`, return indices of the two numbers such that they add up to \`target\`.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
-### Example 1:
-
-\`\`\`
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-\`\`\`
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-### Example 2:
-
-\`\`\`
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-\`\`\`
-
-### Example 3:
-
-\`\`\`
-Input: nums = [3,3], target = 6
-Output: [0,1]
-\`\`\`
-
-### Constraints:
-
-- 2 <= nums.length <= 10^4
-- -10^9 <= nums[i] <= 10^9
-- -10^9 <= target <= 10^9
-- Only one valid answer exists.
-
-### Follow-up:
-Can you come up with an algorithm that is less than O(n²) time complexity?
-`;
 
 export const LANGUAGES: Language[] = [
     { id: 'javascript', name: 'JavaScript' },
@@ -66,16 +25,6 @@ export const DEFAULT_CODE_BY_LANGUAGE: Record<Language['id'], string> = {
     cpp: '// Write your C++ code here\n\n#include <iostream>\n\nint main() {\n    std::cout << "Hello World!" << std::endl;\n    return 0;\n}\n',
     csharp: '// Write your C# code here\n\npublic class Solution {\n    public static void Main(string[] args) {\n        System.Console.WriteLine("Hello World!");\n    }\n}\n',
 };
-
-export const hintContent = [
-    { id: 1, type: "hint", title: "Hint 1", content: "A really brute force way would be to search for all possible pairs of numbers but that would be too slow. Again, it's best to try out brute force solutions for just for completeness. It is from these brute force solutions that you can come up with optimizations." },
-    { id: 2, type: "hint", title: "Hint 2", content: "So, if we fix one of the numbers, say x, we have to scan the entire array to find the next number y which is value - x where value is the input parameter. Can we change our array somehow so that this search becomes faster?" },
-    { id: 3, type: "hint", title: "Hint 3", content: "The second train of thought is, without changing the array, can we use additional space somehow? Like maybe a hash map to speed up the search?" }
-]
-export const topics = [
-    {id: 1, name: "Array"},
-    {id: 2, name: "Hash Table"},
-]
 
 export const problemStatementMenuData = [
     { id: 1, name: "Description", onclick: () => console.log("Description") },
--- src/pages/problem/ProblemStatement.tsx
+++ src/pages/problem/ProblemStatement.tsx
@@ -1,9 +1,9 @@
 import { Badge } from '../../components/ui/badge';
 import { Icon } from '../../components/ui/icon';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../../components/ui/accordion';
-import { hintContent, topics } from '@/lib/constant';
 import MarkdownViewer from '@/components/markdown-viewer/markdown-viewer';
 import { ProblemData } from '@/types';
+import { cn } from '@/lib/utils';
 
 interface MarkdownViewerProps {
     problemData: ProblemData | null
@@ -17,7 +17,7 @@ const ProblemStatement = ({ problemData }: MarkdownViewerProps) => {
                 <h1 className='text-2xl font-semibold mb-4'>{problemData?.title}</h1>
             </div>
             <div className='flex items-center justify-start mb-4 gap-2'>
-                <Badge variant={'outline'} className='rounded-full px-3 py-1 text-easy bg-muted'>{problemData?.difficulty}</Badge>
+                <Badge variant={'outline'} className={cn('rounded-full px-3 py-1 text-easy bg-muted', `text-${problemData?.difficulty.toLowerCase()}`)}>{problemData?.difficulty}</Badge>
                 <Badge variant={'outline'} className='rounded-full px-3 py-1'><Icon name='TagIcon' /> Topics</Badge>
                 <Badge variant={'outline'} className='rounded-full px-3 py-1'><Icon name='LightbulbIcon' /> Hint</Badge>
             </div>
@@ -31,19 +31,20 @@ const ProblemStatement = ({ problemData }: MarkdownViewerProps) => {
                         <AccordionTrigger className='hover:no-underline cursor-pointer'>
                             <div className='flex gap-5 items-center '><Icon name='TagIcon' className='h-4 w-4' />{"Topics"}</div>
                         </AccordionTrigger>
-                        <AccordionContent className='ml-10 gap-2 flex'>{topics.map(item => <Badge className='rounded-full px-3 py-1' variant={'secondary'} key={item.id}>{item.name}</Badge>)}</AccordionContent>
+                        <AccordionContent className='ml-10 gap-2 flex'>{problemData?.tags.map(item => <Badge className='rounded-full px-3 py-1' variant={'secondary'} key={item._id}>{item.name}</Badge>)}</AccordionContent>
                     </AccordionItem>
                 </Accordion>
             </div>
+
             {/* Hint Section */}
             <div>
                 <Accordion type="single" collapsible className="w-full">
                     {
-                        hintContent.map((item) => {
+                        problemData?.hints.map((item, index) => {
                             return (
-                                <AccordionItem key={item.id} value={`item-${item.id}`} className='border-b'>
-                                    <AccordionTrigger className='hover:no-underline cursor-pointer'><div className='flex gap-5 items-center'><Icon name='LightbulbIcon' className='h-4 w-4' />{item.title}</div></AccordionTrigger>
-                                    <AccordionContent className='ml-10'>{item.content}</AccordionContent>
+                                <AccordionItem key={item} value={`item-${index}`} className='border-b'>
+                                    <AccordionTrigger className='hover:no-underline cursor-pointer'><div className='flex gap-5 items-center'><Icon name='LightbulbIcon' className='h-4 w-4' />Hint {index + 1}</div></AccordionTrigger>
+                                    <AccordionContent className='ml-10'>{item}</AccordionContent>
                                 </AccordionItem>
                             )
                         })
