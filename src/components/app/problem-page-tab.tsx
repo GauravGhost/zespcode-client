7c33846c4cd949627b7c9bf8f145b63b62a13d95
Gyanendra Kumar <gyanendrak874@gmail.com>
2025-05-01 15:40:12 +0530

    Added scroll view, if tab get's hidden due to resize

--- src/components/app/problem-page-tab.tsx
+++ src/components/app/problem-page-tab.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react"
 import { Icon } from "../ui/icon"
 import { Separator } from "../ui/separator"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs"
+import { ScrollArea, ScrollBar } from "../ui/scroll-area"
 import * as LucideIcons from "lucide-react"
 
 export type TabItem = {
@@ -30,26 +31,29 @@ const ProblemPageTab = ({
     return (
         <Tabs defaultValue={activeDefaultTab} className={className}>
             <div className="items-center flex bg-muted rounded-t-lg">
-                <TabsList className="h-10 gap-1">
-                    {tabs.map((tab, index) => (
-                        <>
-                            <TabsTrigger
-                                key={`tab-${tab.id}`}
-                                className="cursor-pointer border-none hover:bg-hover rounded-sm"
-                                value={tab.id}
-                            >
-                                {tab.icon && <Icon color={tab.iconColor} name={tab.icon} className="mr-1" />} {tab.label}
-                            </TabsTrigger>
-                            {index < tabs.length - 1 && (
-                                <Separator
-                                    key={`separator-${tab.id}`}
-                                    orientation="vertical"
-                                    className="data-[orientation=vertical]:h-4"
-                                />
-                            )}
-                        </>
-                    ))}
-                </TabsList>
+                <ScrollArea className="w-full">
+                    <TabsList className="h-10 gap-1 inline-flex">
+                        {tabs.map((tab, index) => (
+                            <>
+                                <TabsTrigger
+                                    key={`tab-${tab.id}`}
+                                    className="cursor-pointer border-none hover:bg-hover rounded-sm"
+                                    value={tab.id}
+                                >
+                                    {tab.icon && <Icon color={tab.iconColor} name={tab.icon} className="mr-1" />} {tab.label}
+                                </TabsTrigger>
+                                {index < tabs.length - 1 && (
+                                    <Separator
+                                        key={`separator-${tab.id}`}
+                                        orientation="vertical"
+                                        className="data-[orientation=vertical]:h-4"
+                                    />
+                                )}
+                            </>
+                        ))}
+                    </TabsList>
+                    <ScrollBar orientation="horizontal" className="h-1.5" />
+                </ScrollArea>
             </div>
 
             {tabs.map(tab => (
